621f115a8746001d53d794cd99ef581dcd1acdf0
Mordaina <juliette.imbert@insa-lyon.fr>
2018-04-26 09:47:26 +0200

    Fixed import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -71,5 +71,5 @@ INSERT INTO sport(SPORT_ID, NOM, KMH, KCALH) VALUES (1, 'Course', 12, 880);
 INSERT INTO sport(SPORT_ID, NOM, KMH, KCALH) VALUES (2, 'Marche', 4, 245);
 INSERT INTO sport(SPORT_ID, NOM, KMH, KCALH) VALUES (3, 'Cyclisme', 20, 690);
 
-INSERT INTO programme(programme_id, date_debut, user_id) VALUES (1, '2018-04-15', 100);
-INSERT INTO programme(programme_id, date_debut, user_id) VALUES (2, '2018-04-22', 100);
+INSERT INTO programme(programme_id, date_debut, user_id) VALUES (1, '2018-04-16', 100);
+INSERT INTO programme(programme_id, date_debut, user_id) VALUES (2, '2018-04-23', 100);
