bdc324925c784a860c16037888735212ddddbdcb
Pache <etienne.raquin@insa-lyon.fr>
2018-04-27 10:15:46 +0200

    Algorithm Program Generation In Progress Getting TimeFrame Date

--- src/main/java/smart/Algorithms/ProgramActivities.java
+++ src/main/java/smart/Algorithms/ProgramActivities.java
@@ -1,13 +1,16 @@
 package smart.Algorithms;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import smart.Entities.CentreInteret;
-import smart.Entities.Point;
-import smart.Entities.TimeFrame;
-import smart.Entities.User;
+import smart.Entities.*;
+import smart.Services.CentreInteretService;
+import smart.Services.SportService;
 import smart.Services.TimeFrameService;
 
+import javax.swing.*;
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static java.lang.Math.exp;
 import static java.lang.Math.pow;
@@ -18,40 +21,102 @@ public class ProgramActivities
     @Autowired
     private TimeFrameService timeFrameService;
 
-    final double kDistanceUserToCentreInteretEvaluation = 0.5;
+    @Autowired
+    private SportService sportService;
 
-    public void calculate(User user)
+    @Autowired
+    private CentreInteretService centreInteretService;
+
+    final double kDistanceUserToCentreInteretEvaluation = 0.0002;
+    final double cDistanceUserToCentreInteretEvaluation = 1.0;
+
+    class TimeFrameCentreInteret
+    {
+        private TimeFrame timeFrame;
+        private CentreInteret centreInteret;
+        private double evaluation;
+    }
+
+    public Programme calculate(User user)
     {
         // @TODO For each Sport
         // @TODO Try to split
 
         double objectifHebdo = user.getObjectifHebdo();
         Point userLocation = user.getLocation();
-        Point centreInteretCentre;
-        double note;
+
         double distanceUserToCentreInteret;
         double distanceUserToCentreInteretEvaluation;
+        double centreInteretEvaluation;
+        TimeFrameCentreInteret timeFrameCentreInteret;
+        List <TimeFrameCentreInteret> timeFrameCentreInterets = new ArrayList <TimeFrameCentreInteret> ();
+
+        // @TODO Generalize
+        Iterable <Sport> sports = sportService.getAllSports();
+        Sport course = new Sport();
+        for (Sport sport : sports)
+        {
+            course = sport;
+        }
 
         // For each TimeFrame
         Iterable <TimeFrame> timeFrames = timeFrameService.getTimeFrameAll();
         for (TimeFrame timeFrame : timeFrames)
         {
             // For each CentreInteret
-            Iterable <CentreInteret> centreInterets = centreInteretService.getCentreInteretAll();
+            Iterable<CentreInteret> centreInterets = centreInteretService.getCentreInteretAll();
             for (CentreInteret centreInteret : centreInterets)
             {
-                centreInteretCentre = centreInteret.getCercle().getCentre();
+                timeFrameCentreInteret = new TimeFrameCentreInteret();
+                timeFrameCentreInteret.timeFrame = timeFrame;
+                timeFrameCentreInteret.centreInteret = centreInteret;
 
-                distanceUserToCentreInteret = sqrt(pow(userLocation.getX() - centreInteretCentre.getX(), 2) +
-                    pow(userLocation.getY() - centreInteretCentre.getY(), 2));
+                // Calculate Distance from User to CentreInteret
+                distanceUserToCentreInteret = sqrt(pow(userLocation.getX() - centreInteret.getPoint().getX(), 2) +
+                    pow(userLocation.getY() - centreInteret.getPoint().getY(), 2));
 
+                // Calculate Evaluation of Distance from User to CentreInteret
                 distanceUserToCentreInteretEvaluation = exp(-kDistanceUserToCentreInteretEvaluation * distanceUserToCentreInteret);
 
-                note = timeFrame.getEvaluation() * distanceUserToCentreInteretEvaluation;
+                // Calculate CentreInteret Evaluation
+                centreInteretEvaluation = cDistanceUserToCentreInteretEvaluation * distanceUserToCentreInteretEvaluation / 1.0;
+
+                // Calculate TimeFrameCentreInteret Evaluation
+                timeFrameCentreInteret.evaluation = timeFrame.getEvaluation() * centreInteretEvaluation;
+
+                // Insert TimeFrameCentreInteret in List, sorted by Evaluation
+                int index = 0;
+                while (index < timeFrameCentreInterets.size() && timeFrameCentreInterets.get(index).evaluation > timeFrameCentreInteret.evaluation)
+                {
+                    index = index + 1;
+                }
+                timeFrameCentreInterets.add(index, timeFrameCentreInteret);
             }
         }
 
-        return;
+        
+
+        // Create Activity
+        activity = new Activity();
+        // @TODO
+        // activity.setDate(timeFrame.getDate());
+        activity.setDate(new Date());
+        activity.setDistance((float)objectifHebdo);
+        activity.setSport(course);
+        activity.setEstRealisee(false);
+        //activity.setProgramme();
+
+        // Create Activities
+        activities = new ArrayList <Activity>();
+        activities.add(activity);
+
+        // Create Program
+        programme = new Programme();
+        programme.setUser(user);
+        programme.setActivites(activities);
+        activity.setProgramme(programme);
+
+        return programme;
     }
 
     private ProgramActivities()
