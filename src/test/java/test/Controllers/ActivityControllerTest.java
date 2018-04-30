f4be53f16b7b0f3964fc36d0812d63df968541d4
Victor <vb4007@gmail.com>
2018-04-30 08:59:06 +0200

    Added shouldGetNextActivity test

--- src/test/java/test/Controllers/ActivityControllerTest.java
+++ src/test/java/test/Controllers/ActivityControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,12 +14,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import smart.Application;
+import smart.Controller.CronController;
 import smart.DTO.ActivityDTO;
+import smart.Entities.DonneeAthmospherique;
+import smart.Jwt.JwtTokenUtil;
+import smart.Services.DonneeAthmospheriqueService;
+import smart.Services.TimeFrameService;
+import smart.Services.WeatherDataService;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,8 +40,20 @@ public class ActivityControllerTest {
 
     private MockMvc mvc;
 
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
+
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private WeatherDataService weatherDataService;
+
+    @Autowired
+    private DonneeAthmospheriqueService donneeAthmospheriqueService;
+
+    @Autowired
+    private TimeFrameService timeFrameService;
 
     @Before
     public void setup() {
@@ -73,5 +94,25 @@ public class ActivityControllerTest {
 
         this.mvc.perform(post("/activity/add").contentType(MediaType.APPLICATION_JSON).content(json).accept(MediaType.APPLICATION_JSON))
             .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldGetNextActivity() throws Exception{
+
+        when(jwtTokenUtil.getUsernameFromToken(any())).thenReturn("user10");
+
+        mvc.perform(get("/update")
+            .header("Authorization", "Bearer anyToken"))
+            .andExpect(status().isOk());
+
+        mvc.perform(get("/programme/generate")
+            .header("Authorization", "Bearer anyToken"))
+            .andExpect(status().isOk());
+
+        mvc.perform(get("/activity/nextPlanned")
+            .header("Authorization", "Bearer anyToken"))
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(jsonPath("$.estRealisee").value(false));
     }
 }
