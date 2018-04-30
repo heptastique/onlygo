b347d38726c03ae017f57efbd2516492450f5157
Mordaina <juliette.imbert@insa-lyon.fr>
2018-04-30 10:10:05 +0200

    Tests added

---
+++ src/test/java/test/Controllers/ProgrammeControllerTest.java
@@ -0,0 +1,62 @@
+package test.Controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import smart.Application;
+import smart.Jwt.JwtTokenUtil;
+
+import java.text.SimpleDateFormat;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ProgrammeControllerTest {
+
+    private MockMvc mvc;
+
+    @MockBean
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+            .webAppContextSetup(context)
+            .apply(springSecurity())
+            .build();
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void shouldGetActiveProgrammeOfUser() throws Exception{
+        when(jwtTokenUtil.getUsernameFromToken(any())).thenReturn("user");
+
+        mvc.perform(get("/programme/active").header("Authorization", "Bearer anyToken"))
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(jsonPath("$.id").value("200"));
+    }
+
+    @Test
+    public void shouldGetUnauthorizedWithoutRole() throws Exception{
+        this.mvc
+            .perform(get("/programme/active"))
+            .andExpect(status().isUnauthorized());
+    }
+}
