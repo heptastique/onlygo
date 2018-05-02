118316bab29e1dd458bce959191551687210fde9
Pache <etienne.raquin@insa-lyon.fr>
2018-05-02 07:51:49 +0200

    Clean : user ProgramActivities as Singleton

--- src/main/java/smart/Controller/ProgrammeController.java
+++ src/main/java/smart/Controller/ProgrammeController.java
@@ -32,9 +32,6 @@ public class ProgrammeController {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private ProgramActivities programActivities;
-
     @RequestMapping(path="/programme/active", method = RequestMethod.GET)
     public ResponseEntity<?> getActiveProgrammeOfUser(HttpServletRequest request) {
         // This returns a JSON or XML with the active program of the user (current week and all the activities it contains)
@@ -67,7 +64,7 @@ public class ProgrammeController {
         String token = request.getHeader(tokenHeader).substring(7);
         String username = jwtTokenUtil.getUsernameFromToken(token);
         User user = userRepository.findByUsername(username);
-        Programme programme = programActivities.calculate(user);
+        Programme programme = ProgramActivities.getInstance().calculate(user);
         return ResponseEntity.ok().body(programme);
     }
 }
