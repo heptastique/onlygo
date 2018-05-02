3942ad283e2c1c92063987156f20932d87fa22ea
Victor <vb4007@gmail.com>
2018-05-02 20:31:00 +0200

    Missing import

--- src/test/java/test/Controllers/MainControllerTest.java
+++ src/test/java/test/Controllers/MainControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+
 import smart.Application;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
