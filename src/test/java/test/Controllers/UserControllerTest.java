7c65b1c0def9f21d7bdc5285c2b0c366fcbf4d33
lulucasono <lulucasono@gmail.com>
2018-05-03 11:28:44 +0200

    change user to avoid conflict

--- src/test/java/test/Controllers/UserControllerTest.java
+++ src/test/java/test/Controllers/UserControllerTest.java
@@ -179,14 +179,14 @@ public class UserControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "user40")
+    @WithMockUser(username = "user41")
     public void changeUserPassword() throws Exception{
         String password = "newpassword";
         UserDto userDto= new UserDto();
         userDto.setPassword(password);
         Gson gson = new Gson();
         String json = gson.toJson(userDto,UserDto.class);
-        when(jwtTokenUtil.getUsernameFromToken(any())).thenReturn("user40");
+        when(jwtTokenUtil.getUsernameFromToken(any())).thenReturn("user41");
 
         mvc.perform(put("/user/password")
             .contentType(MediaType.APPLICATION_JSON).header("Authorization","Bearer anyToken").content(json).accept(MediaType.APPLICATION_JSON))
